bd856d212c4a1ffe9bd15df845b6dbdedfec92f0
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-16 11:11:08 +0530

    Update and rename README.md to next.java

---
+++ next.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int num=123,nextNum=0;
+	System.out.println(num);
+	System.out.println("nextNum"+124);
+	}
+}
